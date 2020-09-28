4f300e0ce13bfb4423cedabc3ac6803d49768d75
Ismail Wahba <isma3il.samir@gmail.com>
2020-09-28 11:14:56 +0000

    fixing syntax in prob plot

--- data_ml_utils/evalutils.py
+++ data_ml_utils/evalutils.py
@@ -278,8 +278,8 @@ class xgboost_eval(general_eval):
     
     def get_prob_plot(self):
         df= self.plots['prob']
-        neg= df[df['classification'=='Negatives']['prob_class_1']]
-        pos= df[df['classification'=='Positives']['prob_class_1']]
+        neg= df[df['classification'=='Negatives']]['prob_class_1']
+        pos= df[df['classification'=='Positives']]['prob_class_1']
         plt.hist(neg, bins=100, label='Negatives')
         plt.hist(pos, bins=100, label='Positives', alpha=0.7, color='r')
         plt.xlabel('Probability of being Positive Class')
