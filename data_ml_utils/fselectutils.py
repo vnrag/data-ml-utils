ab4f90209172907cd42660d8f4f23099dabce434
Ismail Wahba <isma3il.samir@gmail.com>
2020-09-29 19:18:24 +0000

    debugging fselect

--- data_ml_utils/fselectutils.py
+++ data_ml_utils/fselectutils.py
@@ -10,6 +10,10 @@ class general_select(main_utils):
     def get_features_corr_matrix(self, matrix):
         x_pd= gu.create_data_frame(matrix)
         corr_mtx= x_pd.corr()
+        ######################################################
+        #should be removed when we have the feature names
+        corr_mtx= corr_mtx.add_prefix('f')
+        ######################################################
         corr_mtx.index.name ='feature'
         corr_mtx.reset_index(level=0, inplace=True)
         self.feature_metrics['corr_matrix'] = corr_mtx
@@ -25,7 +29,5 @@ class general_select(main_utils):
         self.export_df_as_text(df, 'feature_corr_matrix')
     
     def export_corr_matrix_to_s3(self):
-        import IPython
-        IPython.embed()
         df= self.feature_metrics['corr_matrix']
         self.export_metric_to_s3(df, 'feature_corr_matrix', 'feature_corr_matrix')   
