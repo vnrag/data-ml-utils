bd494a3640cd65c13378ae4a3794c9a38ceecf7e
Ismail Wahba <isma3il.samir@gmail.com>
2020-10-19 15:23:59 +0000

    first try integrating with sagemaker

--- data_ml_utils/fselectutils.py
+++ data_ml_utils/fselectutils.py
@@ -10,17 +10,6 @@ class general_select(main_utils):
     def get_features_corr_matrix(self, matrix):
         x_pd= gu.create_data_frame(matrix)
         corr_mtx= x_pd.corr()
-        ######################################################
-        #should be removed when we have the feature names
-        # corr_mtx= corr_mtx.add_prefix('f')
-        ######################################################
-
-        # corr_mtx= corr_mtx.melt()
-        # corr_mtx= corr_mtx.rename(columns={"variable": "feature_1"})
-        # corr_mtx.index.name ='feature'
-        # corr_mtx.reset_index(level=0, inplace=True)
-        # corr_mtx['model']= self.atomic_metrics['model']
-        # corr_mtx['ts']= self.atomic_metrics['ts']
         self.feature_metrics['corr_matrix'] = corr_mtx
     
     def export_metrics(self):
